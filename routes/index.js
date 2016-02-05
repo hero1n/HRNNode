6426687fbf18f14779cafaad0c9680c0a7a4bb09
노재원 <opqw987@gmail.com>
2016-02-05 12:10:03 +0900

    초기화

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json({
+  	a: 1
+  })
+  // res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
